d91501e8734d77556abee1788281d8fddecd74bd
Yash Singh <y.singh2519@gmail.com>
2023-08-16 19:28:21 +0530

    It is a simple 'Random Number Guessing' program which generate a random number then get input from player and tell too heigh or too low, and this process will continue until you will get correct answer.

---
+++ Number Gussing game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<endl<<"Simple Number Gussing Game"<<endl;
+  int rn = rand()%100;
+  
+  again:
+  cout<<endl<<"Enter number b/w 1-100: ";
+  cin>>n;
+  
+  if(rn!=n){
+    
+    rn > n ? cout<<"Two low! "<<rn-n<<endl : cout<<"Two high! "<<n-rn<<endl;
+    goto again;
+    
+  }else{
+    
+    cout<<"Congratulation! You win.";
+    
+  }
+  
+  return 0;
+}
+
